5256c5e6a8bc5ed714f6ec6384eceef3f80ad509
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2020-10-08 22:44:40 +0400

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString().split('').reverse().join('');
+  if (str[str.length-1] === '-'){
+      return +str.substring(0,str.length-1);
+  }
+  else {
+    return +str;
+    }
 }
